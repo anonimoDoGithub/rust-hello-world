74a97e2a21092a4268f8df0843824367a8a1a40f
anonimoDoGithub <xxexpgamerxx@gmail.com>
2023-11-12 20:40:08 -0300

    adicionando comentario ao tipos_de_dados_00

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,17 @@
 fn main() {
-    let nome = "Sergio";
-    let sobrenome: &str = "Felipe";
-    let idade: i8 = 37;
-    let altura: f32 = 1.75;
-    let estudando: bool = true;
-    let mut pais = "Brazil"; 
-    //pais = "Canadá";
-
-    println!("Seja bem vindo, {} {}!", nome, sobrenome);
-    println!("Sua idada atual é: {}anos.", idade);
-    println!("Sua altura atual é: {}cm.", altura);
-    println!("No momento você está aprendendo sobre: [{}] RUST.", estudando);
-    println!("Seu país de atual é: {}.", pais);
+    let pais = "Brazil";
+    let ano: i16 = 2023;
+    let populacao: f32 = 219.4;
+    let povo: bool = true;
+    
+    println!("Seja bem vindo, ao {}!", pais);
+    println!("Ano atual, {}.", ano);
+    println!("População atual, {} milhões de habitantes.", populacao);
+    println!("Um povo trabalhador, 100% {}.", povo);
+    
 }
 
-/* Variáveis
-    Em RUST todas as variáveis são por padrão IMUTAVEIS ou seja não podem ter seus valores REATRIBUIDOS.
-    *Porém, caso seja necessário podemos declarar a variável como MUTAVEL usando  "mut" antes do nome da variável.
-    Exemplo: let mut pais = "Brazil";
-    ----
-    Não precisamos explicitar qual o tipo de dado, em nossas variáveis.
-    O Rust é uma linguagem de programação que suporta inferência de tipo.
-    *Porém a inferência de tipo no Rust ajuda a tornar o código mais conciso e legível,
-    enquanto ainda mantém a segurança de tipo que é uma característica fundamental da linguagem.
-    ----
-    Usamos a sintaxe {} para inserir uma variável, dentro de uma STRING.
-    Exemplo: println!("Sua idada atual é: {}anos.", idade);
+/* Tipos de Dados
+    - INTEIROS - 
+
 */
