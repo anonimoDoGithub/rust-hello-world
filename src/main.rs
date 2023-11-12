c45686540b577a2200a2a95fc8223fd3dac3e290
anonimoDoGithub <xxexpgamerxx@gmail.com>
2023-11-12 13:58:02 -0300

    adicionando notações de variaveis a branch: variavel_00

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
     let nome = "Sergio";
-    println!("Hello, world!");
-    println!("Seja bem vindo, {}", nome);
+    let sobrenome: &str = "Felipe";
+    let idade: i8 = 37;
+    let altura: f32 = 1.75;
+    let estudando: bool = true;
+
+    println!("Seja bem vindo, {} {}!", nome, sobrenome);
+    println!("Sua idada atual é: {}.", idade);
+    println!("Sua altura atual é: {}.", altura);
+    println!("No momento você está aprendendo sobre: [{}] RUST.", estudando);
 }
