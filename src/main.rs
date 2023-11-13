35ac6c2164b156ee264035e582fb7f45d0cf549b
anonimoDoGithub <xxexpgamerxx@gmail.com>
2023-11-12 23:52:15 -0300

    adicionando comentario ao tipos_de_dados_00

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    let pais = "Brazil";
+    let pais: &str = "Brazil";
+    let sigla: char = 'S';
     let ano: i16 = 2023;
     let populacao: f32 = 219.4;
     let povo: bool = true;
@@ -7,11 +8,19 @@ fn main() {
     println!("Seja bem vindo, ao {}!", pais);
     println!("Ano atual, {}.", ano);
     println!("População atual, {} milhões de habitantes.", populacao);
-    println!("Um povo trabalhador, 100% {}.", povo);
+    println!("Um povo trabalhador, 100% {} {}.", povo, sigla);
     
 }
 
 /* Tipos de Dados
-    - INTEIROS - 
+    - INTEIROS - i32(32bits)Default
+    signed   - i8(8bits)|i16(16bits)|i32(32bits)|i64(64bits)|i128(128bits) *Permitem números positivos e negativos!
+    unsigned - u8(8bits)|u16(16bits)|u32(32bits)|u64(64bits)|u128(128bits) *Não permite números negativos!
 
+    - FLUTUANTES - f32(32bits)
+    Existem dois tipos de pontos flutuantes: f32(32bits) e f64(64bits)
+    f64 - utilizado para DUPLA PRECISÃO;
+    f32 - utilizado para SINGLE PRECISÃO;
+
+    - BOOLEANS - true ou false
 */
