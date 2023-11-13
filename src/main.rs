e2e8ca9cd7f8a612f913fd3021246c366821972f
anonimoDoGithub <xxexpgamerxx@gmail.com>
2023-11-13 06:16:33 -0300

    adicionando ao fluxo_de_controle_01

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-fn main() {
-    let velocidade1: u16 = 100;
+use std::io;
 
-    if velocidade1 < 100 && velocidade1 >= 80 {
-        println!("Diminua a velocidade!")
-    }else if velocidade1 >= 100 {
-        println!("Multa por excesso de velocidade!")
+fn convert_to_int(data_input: &String) -> i32 {
+    let x = data_input.trim().parse::<i32>().unwrap();
+    x
+}
+
+fn main() {
+    let mut number1 = String::new();
+    io::stdin().read_line(&mut number1).expect("Erro ao ler number1");
+    let mut number2 = String::new();
+    io::stdin().read_line(&mut number2).expect("Erro ao ler number1");
+
+    if convert_to_int(&number1) > convert_to_int(&number2){
+        println!("O número {} é maior que o número {}!", number1,number2);
     }else {
-        println!("Sem excesso de velocidade!")
+        println!("O número {} é maior ou igual ao número {}!", number1,number2);
     }
-    
 }
